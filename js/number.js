fa95823ffe67c65aec30cd1aee49453cb3a4bf95
Fernando Cardozo <nuevo_10000@outlook.com>
2023-11-30 10:32:20 -0300

    feat: floating numbers and scientific notation

--- js/number.js
+++ js/number.js
@@ -14,3 +14,13 @@ let c = 3;      // Variables.
 // The rest are operators.
 let result = 56 * c + a / (a ** 2 - 43 - (11 + b)); // Expressions, literals, and variables.
 // ------------------ //
+// You can use floating numbers and they are always 64 bits floating point:
+// And you can express numbers as scientific notation:
+let numberScientificNotation = 123e5; // 12300000
+let floatingNumber = 3.141516; // It's not PI number.
+// The maximum level of decimals is 17.
+// Floating point arithmetic is not always 100% accurate.
+// Scientific notation:
+console.log(numberScientificNotation);
+// Floating number:
+console.log(floatingNumber);
