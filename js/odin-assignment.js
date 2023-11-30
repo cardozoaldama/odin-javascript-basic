210e4bb61ef3b7a57926c100acb2f639eedf3402
Fernando Cardozo <nuevo_10000@outlook.com>
2023-11-30 12:51:33 -0300

    fix: delete some unnecessary tab spaces

--- js/odin-assignment.js
+++ js/odin-assignment.js
@@ -10,19 +10,19 @@ Try the following exercises (and don’t forget to use console.log()!):
 3. Print the solution to the following equation: (4 + 6 + 9) / 77
     - Answer should be approximately 0.24675
 
-    4. Let’s use variables!
+4. Let’s use variables!
     - Type this statement at the top of the script tag: let a = 10
     - In the console console.log(a) should print 10
     - Try the following in the console: 9 * a
     - And this: let b = 7 * a (returns undefined *) and then console.log(b)
 
-    5. You should be getting the hang of this by now… try this sequence:
+5. You should be getting the hang of this by now… try this sequence:
     - Declare a constant variable MAX with the value 57
     - Set another variable actual to MAX - 13
     - Set another variable percentage to actual / MAX
     - If you type percentage in the console and press Enter you should see a value like 0.7719
 
-    6. Take a few minutes to keep playing around with various things in your script tag. 
+6. Take a few minutes to keep playing around with various things in your script tag. 
 Eventually, we will learn how to actually make those numbers and things show up on the webpage, 
 but all of this logic will remain the same, so make sure you’re comfortable with it before moving on.
 
