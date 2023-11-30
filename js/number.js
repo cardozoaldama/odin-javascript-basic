3cba58ff336995d03be9b4ef97efb55a27e6d53c
Fernando Cardozo <nuevo_10000@outlook.com>
2023-11-30 10:25:48 -0300

    feat: adding operators and operands in expressions
    Arithmetic with variables, literals, and expressions. Also, you can watch these "result" variable with precedence order.

--- js/number.js
+++ js/number.js
@@ -5,3 +5,12 @@
 // A little introduction:
 console.log('NUMBERS:');
 // ------------------ //
+/* You can use arithmetic with variables, literals, or expressions. */
+let a = 56;     // Variable.
+let b = 22;     // Variable.
+let c = 3;      // Variables.
+// The result:
+// These operand are: 56, c, 43, a, 11, and b.
+// The rest are operators.
+let result = 56 * c + a / (a ** 2 - 43 - (11 + b)); // Expressions, literals, and variables.
+// ------------------ //
