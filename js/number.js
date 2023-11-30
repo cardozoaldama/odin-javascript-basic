ddc99a7b4860d82cd8306fc0dc20f30c6a993778
Fernando Cardozo <nuevo_10000@outlook.com>
2023-11-30 10:21:04 -0300

    feat: number.js added and topic about numbers

---
+++ js/number.js
@@ -0,0 +1,7 @@
+/* WE ARE GOING TO FIGHT! */
+// We got numbers here.
+// The JavaScript arithmetic operators.
+// +, -, *, /, %, ** (ES2016), ++, --
+// A little introduction:
+console.log('NUMBERS:');
+// ------------------ //
