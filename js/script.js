1166b8d7ac10fde31bb30e88ebdca8e44603fc16
Fernando Cardozo <nuevo_10000@outlook.com>
2023-11-30 08:44:57 -0300

    feat: JS folder with script.js and console.log()

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// This is the external JavaScript file, for more complex scripts.
+// Our dear console:
+console.log('Hello world from the JavaScript file (external)!');
+
+// VARIABLES:
+// They are kind of storage containers for data in our code:
+// In order to create variables in JavaScript, the only way was `var` statement.
+// Nowadays we got two more ways to create variables: `const` and `let`.
