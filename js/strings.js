c0b30046c4dcc4e6358328f4cbfc59ef322a1475
Fernando Cardozo <nuevo_10000@outlook.com>
2023-12-01 11:33:05 -0300

    feat: strings JS file added with some comments
    In this file, we are going to fill exercises about the string and its functions.

---
+++ js/strings.js
@@ -0,0 +1,22 @@
+/* STRINGS IN JAVASCRIPT! */
+// We are going to use string data type with some functions just right here.
+// These some commons functions:
+/**
+String length
+String slice()
+String substring()
+String substr()
+String replace()
+String replaceAll()
+String toUpperCase()
+String toLowerCase()
+String concat()
+String trim()
+String trimStart()
+String trimEnd()
+String padStart()
+String padEnd()
+String charAt()
+String charCodeAt()
+String split()
+*/
