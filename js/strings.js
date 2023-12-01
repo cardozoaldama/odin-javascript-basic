63babb7783c6397fb47ce60aea52b113f6dcae89
Fernando Cardozo <nuevo_10000@outlook.com>
2023-12-01 12:02:45 -0300

    feat: replacing string content exercise
    Using functions such as replace() and replaceAll() (from ES2021) and it is working!

--- js/strings.js
+++ js/strings.js
@@ -37,3 +37,19 @@ console.log(abecedary.length); // This abecedary has 26 letters.
 console.log(firstName.slice(0, 7));  // It returns "Calamar". The seven (end) is not included.
 console.log(lastName.substring(0, 7)); // It returns... "Calamar" too!
 console.log(abecedary.substr(0, 7)); // It returns: ABCDEFG. It is deprecated.
+
+/* REPLACING STRING CONTENT: */
+let welcomeBack = 'Welcome pal, friend!';
+// Using the function and it returns a string. It doesn't change its string.
+// Notice that our replace() is case sensitive.
+let newWelcomeBack = welcomeBack.replace('pal', 'back'); // Welcome back, friend!
+// Printing!
+console.log(newWelcomeBack);
+// You could use also regular expressions to enhance your replace() method.
+newWelcomeBack = welcomeBack.replace('PAL', 'backend'); // It doesn't work.
+console.log(newWelcomeBack); // Welcome pal, friend! (Same as welcomeBack variable)
+// If you really want to, we can omit case sensitive with a regular expression: /i
+newWelcomeBack = welcomeBack.replace(/PAL/i, 'backend'); // Without using quotes when you are using regular expressions.
+console.log(newWelcomeBack); // Now it works! Welcome backend, friend!
+// You can also use "replaceAll()" and JavaScript introduces it in 2021.
+console.log(welcomeBack.replaceAll('friend', 'FRIEND')); // For me, it's working.
