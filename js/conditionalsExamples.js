c4e093e09ff574f2ec92943766ca19ca68366007
Fernando Cardozo <nuevo_10000@outlook.com>
2023-12-04 10:52:14 -0300

    feat: learning with set weather in JS
    Using HTML tags such as select, option, and a paragraph. Also, using events, and something new about variables with dots attach to them.

--- js/conditionalsExamples.js
+++ js/conditionalsExamples.js
@@ -1,2 +1,42 @@
 /* CONDITIONALS EXAMPLE */
 // Here we are going to practice having some fun:
+// The select tag with its options:
+// Creating two constants.
+// Using this "document.querySelector()" to acquired our tags.
+const select = document.querySelector('select');
+const para = document.querySelector('p');
+// We are going to add an Event Listener:
+// And the "setWeather" is not created yet.
+// Also, the "select" is our select over there and will trigger an event if something is "changed":
+select.addEventListener('change', setWeather);
+// The function "setWeather()"
+function setWeather()
+{
+    // Creating the constant "choice"!
+    // The assignment value is "select.value".
+    // "select.value" is... Our select over there, but precisely its actual value.
+    const choice = select.value;
+    // In these cases, we are using the === operator.
+    // para.textContent means: Put a string inside of this variable (para)
+    if (choice === 'sunny')
+    {
+        para.textContent = 'It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.';
+    }
+    else if (choice === 'rainy')
+    {
+        para.textContent = 'Rain is falling outside; take a rain coat and an umbrella, and don\'t stay out for too long.';
+    }
+    else if (choice === 'snowing')
+    {
+        para.textContent = 'The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.';
+    }
+    else if (choice === 'overcast')
+    {
+        para.textContent = 'It isn\'t raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.';
+    }
+    // If we got nothing, just an empty string.
+    else
+    {
+        para.textContent = '';
+    }
+}
