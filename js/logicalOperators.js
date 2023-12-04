4557b3e59bceaba192ff2f31ed170bf46738f481
Fernando Cardozo <nuevo_10000@outlook.com>
2023-12-04 09:07:13 -0300

    feat: add logical operators JS file
    Also, add a script tag with src to this JS file. The purpose of this JS file is explain the logical operators and some examples.

---
+++ js/logicalOperators.js
@@ -0,0 +1,9 @@
+/* Logical operators */
+// There are four logical operators in JavaScript: AND, OR, NOT, And the Nullish Coalescing.
+// The AND (&&)
+// The OR (||)
+// The NOT (!)
+// And the nullish coalescing (??).
+console.log('LOGICAL OPERATORS!')
+
+// START
