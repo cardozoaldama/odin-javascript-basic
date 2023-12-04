85947ac353820d69c093d7de0b9fabcee0be835c
Fernando Cardozo <nuevo_10000@outlook.com>
2023-12-04 11:27:03 -0300

    feat: new example using ternary operator
    The little example is using the ternary operator and the theme of HTML and CSS, mostly.

--- js/conditionalsExamples.js
+++ js/conditionalsExamples.js
@@ -4,13 +4,13 @@
 // Creating two constants.
 // Using this "document.querySelector()" to acquired our tags.
 const select = document.querySelector('select');
-const para = document.querySelector('p');
+const para = document.querySelector('p'); // When its value is changed, the "setWeather()" function is run.
 // We are going to add an Event Listener:
 // And the "setWeather" is not created yet.
 // Also, the "select" is our select over there and will trigger an event if something is "changed":
 select.addEventListener('change', setWeather);
 // The function "setWeather()"
-function setWeather()
+function setWeather() // Added with addEventListener.
 {
     // Creating the constant "choice"!
     // The assignment value is "select.value".
@@ -35,8 +35,38 @@ function setWeather()
         para.textContent = 'It isn\'t raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.';
     }
     // If we got nothing, just an empty string.
+    // This is the final wave, fight!
     else
     {
         para.textContent = '';
     }
 }
+
+/* Using ternary operator (?) */
+// We cannot define "select" again, we did before. Let's use it again.
+// const select = document.querySelector('select');
+// What we can do is this:
+const selectTheme = document.getElementById('theme');
+// This one, yes, we need to create it. We need to select our HTML.
+const html = document.querySelector('html');
+// This code below said "Select the padding style of the body document... Set the value to 10px."
+// The "document.body" selects the begin and end of your BODY.
+document.body.style.padding = '10px';
+// The function UPDATE.
+// This function needs two arguments, bgColor, and textColor:
+function update(bgColor, textColor)
+{
+    // We are going to use "style" background-color for the HTML.
+    html.style.backgroundColor = bgColor;
+    // Also, set the style color to "textColor" for HTML.
+    html.style.color = textColor;
+}
+// The end of this function.
+// Using "addEventListener" here. When "select" changes its value:
+selectTheme.addEventListener('change', () => // I don't know what it does mean.
+    // Using the ternary operator.
+    selectTheme.value === 'black'        // Condition.
+    ? update('black', 'white')      // TRUE!
+    : update('white', 'black'),     // FALSE!
+);
+// End of this example.
