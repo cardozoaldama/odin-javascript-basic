f19ce4395914546ffc944d753f4d47082c7a5e70
Fernando Cardozo <nuevo_10000@outlook.com>
2023-12-20 10:36:09 -0300

    feat: add sum2 arrow function
    The sum2 arrow function is the same as sum, but using arrow function
    instead the regular function.

--- js/moreFunctions.js
+++ js/moreFunctions.js
@@ -1,11 +1,21 @@
 /* MORE FUNCTIONS IN JAVASCRIPT */
 // Various functions:
 
+// -------
 // A simple "sum" function returning the sum...
+// Also, this is a normal function:
 function sum(a, b)
 {
     return a + b;
 }
+
+// The arrow Function of SUM:
+// Now it is called "sum2"
+let sum2 = (a, b) => a + b;
+// This "sum2" is exactly as "sum", just using arrow functions.
+// We don't use curly braces and return statement, because it assumes we want to return "a + b".
+
+// ---------
 
 // Returning a boolean value there.
 function isPositive(number)
