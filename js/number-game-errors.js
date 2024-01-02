78097be45781cc4c48baa2288a23a1ac5f6969f3
Fernando Cardozo <nuevo_10000@outlook.com>
2024-01-02 14:18:27 -0300

    feat: document formatted number-game-errors.js

--- js/number-game-errors.js
+++ js/number-game-errors.js
@@ -14,25 +14,25 @@ let resetButton;
 function checkGuess() {
 
   const userGuess = Number(guessField.value);
-  if(guessCount === 1) {
+  if (guessCount === 1) {
     guesses.textContent = 'Previous guesses: ';
   }
   guesses.textContent += userGuess + ' ';
 
-  if(userGuess === randomNumber) {
+  if (userGuess === randomNumber) {
     lastResult.textContent = 'Congratulations! You got it right!';
     lastResult.style.backgroundColor = 'green';
     lowOrHi.textContent = '';
     setGameOver();
-  } else if(guessCount === 10) {
+  } else if (guessCount === 10) {
     lastResult.textContent = '!!!GAME OVER!!!';
     setGameOver();
   } else {
     lastResult.textContent = 'Wrong!';
     lastResult.style.backgroundColor = 'red';
-    if(userGuess < randomNumber) {
+    if (userGuess < randomNumber) {
       lowOrHi.textContent = 'Last guess was too low!';
-    } else if(userGuess > randomNumber) {
+    } else if (userGuess > randomNumber) {
       lowOrHi.textContent = 'Last guess was too high!';
     }
   }
@@ -44,29 +44,29 @@ function checkGuess() {
 guessSubmit.addeventListener('click', checkGuess);
 
 function setGameOver() {
-    guessField.disabled = true;
-    guessSubmit.disabled = true;
-    resetButton = document.createElement('button');
-    resetButton.textContent = 'Start new game';
-    document.body.appendChild(resetButton);
-    resetButton.addeventListener('click', resetGame);
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+  resetButton = document.createElement('button');
+  resetButton.textContent = 'Start new game';
+  document.body.appendChild(resetButton);
+  resetButton.addeventListener('click', resetGame);
 }
 
 function resetGame() {
-    guessCount = 1;
+  guessCount = 1;
 
   const resetParas = document.querySelectorAll('.resultParas p');
   for (const resetPara of resetParas) {
     resetPara.textContent = '';
   }
-    resetButton.parentNode.removeChild(resetButton);
+  resetButton.parentNode.removeChild(resetButton);
 
-    guessField.disabled = false;
-    guessSubmit.disabled = false;
-    guessField.value = '';
-    guessField.focus();
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = '';
+  guessField.focus();
 
-    lastResult.style.backgroundColor = 'white';
+  lastResult.style.backgroundColor = 'white';
 
-    randomNumber = Math.floor(Math.random()) + 1;
+  randomNumber = Math.floor(Math.random()) + 1;
 }
