f7617e6d2f2b477e3aa18b7470ef8a7858ff407e
Fernando Cardozo <nuevo_10000@outlook.com>
2024-01-02 10:26:24 -0300

    feat: understanding errors with Stack Trace
    The example is a function, where two of them is calling each other with
    an undefined variable.

--- js/understanding-errors.js
+++ js/understanding-errors.js
@@ -2,11 +2,48 @@
 console.log('Understanding Errors');
 
 // Trying to throw an error:
-const a = 'Hello';
-const b = 'World';
+// const a = 'Hello';
+// const b = 'World';
 // This will probably throw an error:
-console.log(c);
+// console.log(c);
 // First, this is the error:
 // Uncaught ReferenceError: c is not defined.
 // The type of the error is "ReferenceError";
 // Also, the browsers will tell you the file which the error occur and the line (with the character);
+
+/* ANOTHER IMPORTANT ERROR: Stack Trace */
+// Example:
+
+console.log('Stack Trace errors');
+
+// Variable declarations and assignment:
+let x = 5;
+let y = 10;
+
+// The function add() returns "z" variable...
+function add()
+{
+    return z;
+}
+
+// The function print() calls add() function when it invokes:
+function print()
+{
+    add();
+}
+
+// Trying to use the function print();
+print();
+
+// These are the errors:
+/**
+ * Uncaught ReferenceError: z is not defined
+    add ./js/understanding-errors.js:X
+    print ./js/understanding-errors.js:X
+    <anonymous> ./js/understanding-errors.js:X
+    understanding-errors.js:X:Y
+*/
+
+/**
+ * The Stack Trace lets you trace the evolution of an error back to its origin, which here is the declaration of add();
+ */
