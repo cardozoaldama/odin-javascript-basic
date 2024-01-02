0fc607a7d907ea4b86ee3fac9262dc4f68245377
Fernando Cardozo <nuevo_10000@outlook.com>
2024-01-02 10:15:49 -0300

    feat: understanding throw errors
    The "throw" error is: ReferenceError: something is not defined.

--- js/understanding-errors.js
+++ js/understanding-errors.js
@@ -1,2 +1,12 @@
 // Understanding Errors and Warnings...
 console.log('Understanding Errors');
+
+// Trying to throw an error:
+const a = 'Hello';
+const b = 'World';
+// This will probably throw an error:
+console.log(c);
+// First, this is the error:
+// Uncaught ReferenceError: c is not defined.
+// The type of the error is "ReferenceError";
+// Also, the browsers will tell you the file which the error occur and the line (with the character);
