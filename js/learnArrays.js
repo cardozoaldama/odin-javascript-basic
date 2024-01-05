32d13e73f1804ff2fccd87c2af917d53c6246abf
Fernando Cardozo <nuevo_10000@outlook.com>
2024-01-05 11:12:35 -0300

    feat: small definition about arrays

--- js/learnArrays.js
+++ js/learnArrays.js
@@ -1,3 +1,12 @@
 /* We are going to learn about Arrays... */
 // Code begins:
 console.log('Welcome to the world of Arrays!');
+
+// An array is a special variable, which can hold more than one value:
+const gtaCars = ['Banshee', 'ZR-350', 'Sultan'];
+// This array is better than:
+/**
+ * let car1 = 'Banshee';
+ * let car2 = 'ZR-350';
+ * let car3 = 'Sultan';
+ */
