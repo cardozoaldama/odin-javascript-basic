ee7f7ee72baad174f2cff7eb659c48f1aa4c626c
Fernando Cardozo <nuevo_10000@outlook.com>
2024-01-05 17:30:54 -0300

    feat: function draw() for drawing circles
    This function draws a circle 100 times in random positions, mostly.

--- js/loops.js
+++ js/loops.js
@@ -31,3 +31,33 @@ function random(number)
     // Return the result:
     return result;
 }
+// Create a function called draw() which does not return nothing.
+// This is like a procedure.
+function draw()
+{
+    // Creating in context a rectangular area to transparent black.
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // For "i" from 0 until 100, repeat each step with +1:
+    for(let i = 0; i < 100; i++)
+    {
+        // Begin the context path:
+        ctx.beginPath();
+        // Fill with color the context path:
+        ctx.fillStyle = 'rgb(255 255 102 / 50%)';       // rgb(255, 255, 102);
+        // Create an arc or circular arc to our context path:
+        ctx.arc(
+            // The X coordinate with the width of our canvas:
+            random(canvas.width),
+            // The Y coordinate with the height of our canvas:
+            random(canvas.height),
+            // The arc's radius value, like 50 degrees?:
+            random(50),
+            // The start of the angle:
+            0,
+            // The end of the angle:
+            2 * Math.PI
+        );
+        // Now, we fill the context canvas with that drawing.
+        ctx.fill();
+    }
+}
