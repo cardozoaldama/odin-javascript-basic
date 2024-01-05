2ceaa1656246564bd2b1fe29af216f70e817cd0d
Fernando Cardozo <nuevo_10000@outlook.com>
2024-01-05 17:17:34 -0300

    feat: add canvas and a button, with some script
    The script is not completed and I assigned some elements and get some
    others. And the event listener is created when the DOM content has been
    loaded. That even obtains the width and height of client document.

--- js/loops.js
+++ js/loops.js
@@ -1,3 +1,25 @@
 /* Looping Code! */
 console.log('Looping code in JavaScript');
 // Start here:
+
+/* LOOPS */
+// Computers are good for rapidly completing repetitive task.
+// Loops are useful, because they are all about doing the same thing.
+// Same thing over, and over, and over again.
+// We are going to show an example from MDN:
+
+/* Drawing 100 random circles in the screen. */
+// Declare a constant button selecting from the document the button element.
+const btn = document.querySelector('button');
+// Declare a constant canvas selecting from the document the canvas element.
+const canvas = document.querySelector('canvas');
+// Declare a context for the actual constant canvas in 2D.
+const ctx = canvas.getContext('2d');
+// Add an event where the content is loaded, invoke the arrow function inside:
+document.addEventListener('DOMContentLoaded', () =>
+{
+    // The width of our canvas, get from the width of the client document.
+    canvas.width = document.documentElement.clientWidth;
+    // The height of our canvas, get from the height of the client document.
+    canvas.height = document.documentElement.clientHeight;
+});
