8d025652dc70069b15cc83479e4302bba08c9eca
Fernando Cardozo <nuevo_10000@outlook.com>
2024-01-05 17:38:29 -0300

    style: formatting the code with personal style
    Also, added some comments, again.

--- js/loops.js
+++ js/loops.js
@@ -8,26 +8,14 @@ console.log('Looping code in JavaScript');
 // Same thing over, and over, and over again.
 // We are going to show an example from MDN:
 
-/* Drawing 100 random circles in the screen. */
-// Declare a constant button selecting from the document the button element.
-const btn = document.querySelector('button');
-// Declare a constant canvas selecting from the document the canvas element.
-const canvas = document.querySelector('canvas');
-// Declare a context for the actual constant canvas in 2D.
-const ctx = canvas.getContext('2d');
-// Add an event where the content is loaded, invoke the arrow function inside:
-document.addEventListener('DOMContentLoaded', () =>
-{
-    // The width of our canvas, get from the width of the client document.
-    canvas.width = document.documentElement.clientWidth;
-    // The height of our canvas, get from the height of the client document.
-    canvas.height = document.documentElement.clientHeight;
-});
+/* FUNCTIONS */
 // Create a function called random() which returns a result.
 function random(number)
 {
     // Multiply random (i.e. 0.53452542) with X (number).
-    const result = Math.floor(Math.random() * number);
+    const result = Math.floor(
+        Math.random() * number
+    );
     // Return the result:
     return result;
 }
@@ -36,7 +24,14 @@ function random(number)
 function draw()
 {
     // Creating in context a rectangular area to transparent black.
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(
+        // The coordinate X:
+        0, 
+        // The coordinate Y:
+        0, 
+        canvas.width, 
+        canvas.height
+    );
     // For "i" from 0 until 100, repeat each step with +1:
     for(let i = 0; i < 100; i++)
     {
@@ -61,6 +56,20 @@ function draw()
         ctx.fill();
     }
 }
-
+/* Drawing 100 random circles in the screen. */
+// Declare a constant button selecting from the document the button element.
+const btn = document.querySelector('button');
+// Declare a constant canvas selecting from the document the canvas element.
+const canvas = document.querySelector('canvas');
+// Declare a context for the actual constant canvas in 2D.
+const ctx = canvas.getContext('2d');
+// Add an event where the content is loaded, invoke the arrow function inside:
+document.addEventListener('DOMContentLoaded', () =>
+{
+    // The width of our canvas, get from the width of the client document.
+    canvas.width = document.documentElement.clientWidth;
+    // The height of our canvas, get from the height of the client document.
+    canvas.height = document.documentElement.clientHeight;
+});
 // Add an event to the button element with 'click' and invoke the draw() function.
 btn.addEventListener('click', draw);
