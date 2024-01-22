0cbd66bb8fc08d64a47204f0ca7ac912fc879d5b
Fernando Cardozo <nuevo_10000@outlook.com>
2024-01-22 17:00:04 -0300

    feat: using DOM attributes and methods
    I am using about creating, erasing, modifying, some nodes from my DOM in dom.html.

---
+++ js/domMaterial.js
@@ -0,0 +1,29 @@
+// DOM manipulation and Events
+
+// Acquire the container of the document.
+const container = document.querySelector('.container');
+// Show the first element of the .container.
+console.dir(container.firstElementChild);
+// Obtaing controls of the document.
+const controls = document.querySelector('.controls');
+// Show now the sibling of .controls.
+console.dir(controls.previousElementSibling);
+// Create a div element from a div constant.
+const div = document.createElement('div');
+// Append elements:
+container.appendChild(div);
+// You could remove it.
+container.removeChild(div);
+// You can append a new element like this:
+container.insertBefore(div, controls);
+// You can add and manipulate inline CSS styles.
+div.style.backgroundColor = 'blue';
+div.style.height = '200px';
+div.style.width = '200px';
+// It works in that way and you cannot merge CSS kebab-cased with camelCase.
+
+// You can append an attribute for your nodes.
+div.setAttribute('id', 'theDiv');
+console.dir(div.getAttribute('style'));
+div.textContent = 'This is a new div from JavaScript.';
+div.innerHTML = '<span>This is my generated span...</span>';
