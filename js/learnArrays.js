5d330f56479115a84157fe3e6a18fe218a6f18fc
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-05 11:45:50 -0300

    Refactor example map() within console.log

--- js/learnArrays.js
+++ js/learnArrays.js
@@ -260,5 +260,4 @@ function sumOfTripledEvens(array) {
 }
 
 const arr = [1, 2, 3, 4, 5];
-const mappedArr = arr.map(num => num + 1);
-console.log(mappedArr); // Outputs [2, 3, 4, 5, 6]
+console.log(arr.map(num => num + 1)); // Outputs [2, 3, 4, 5, 6]
