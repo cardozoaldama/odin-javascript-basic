a945d3f9b7e9ff37a2ec5b26f9b31f669b7521a5
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-05 11:44:41 -0300

    Add example method to +1 each array element

--- js/learnArrays.js
+++ js/learnArrays.js
@@ -258,3 +258,7 @@ function sumOfTripledEvens(array) {
     }
     return sum;
 }
+
+const arr = [1, 2, 3, 4, 5];
+const mappedArr = arr.map(num => num + 1);
+console.log(mappedArr); // Outputs [2, 3, 4, 5, 6]
