d13140e5d78627c4ecfb35116f2a3719cf21deb7
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-05 11:41:28 -0300

    Add function for sum tripled even numbers

--- js/learnArrays.js
+++ js/learnArrays.js
@@ -243,3 +243,18 @@ console.table(gtaWeapons.slice(2));
 // You can define the start and the end of your slice.
 console.table(gtaBox.slice(0, 2));
 // That is all for arrays, soon we gonna learn about loops.
+
+function sumOfTripledEvens(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        // Step 1: If the element is an even number
+        if (array[i] % 2 === 0) {
+            // Step 2: Multiply this number by three
+            const tripleEvenNumber = array[i] * 3;
+
+            // Step 3: Add the new number to the total
+            sum += tripleEvenNumber;
+        }
+    }
+    return sum;
+}
